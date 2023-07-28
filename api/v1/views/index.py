b3fab2c85b1d121aca156552b3df74c99f23df0f
Mwarstep <mvoi811@gmail.com>
2023-07-28 05:16:18 -0700

    Views Files Update v.3

---
+++ api/v1/views/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""The index module for flask app"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def api_status():
+    """It returns api status"""
+
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def api_stats():
+    """Will retrieve the number of object by type"""
+
+    return jsonify({
+                    "amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                    })
