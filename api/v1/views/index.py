14746c2e35e14da9d4b3d7714337e0a7ba71b3a1
Arnold Mwarstep <111042323+Mwarstep@users.noreply.github.com>
2023-07-28 10:57:28 +0300

    Views v.2

---
+++ api/v1/views/index.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Index File that imports app_views from api.v1.views"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+from models import storage
+
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    This is the status route
+    :return: The JSON response
+    """
+    data = {
+        "status": "OK"
+        }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    This is the stats for all objs route
+    :return: All objs' JSON
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
